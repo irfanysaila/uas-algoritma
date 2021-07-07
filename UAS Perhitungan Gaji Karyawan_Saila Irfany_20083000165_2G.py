ceaa71f75beff70d50df566e9b38d8461da1902f
irfanysaila <85855501+irfanysaila@users.noreply.github.com>
2021-07-07 21:22:36 +0700

    Saila Irfany_20083000165_2G
    Saila Irfany_20083000165_2G

---
+++ UAS Perhitungan Gaji Karyawan_Saila Irfany_20083000165_2G.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jul  7 20:52:13 2021
+
+@author: Disnakerpmptsp_PM1
+"""
+
+print("========================")
+print("Nama             = Bagus")
+print("Golongan         = 1")
+print("Jenis Kelamin    = Laki-laki")
+print("Status Kawin     = Kawin")
+print("Gaji Pokok       = 2500000")
+print("Tunjangan Istri  = 25000")
+print("Tunjangan Anak   = 50000")
+print(">>Gaji Bruto     = 2575000")
+print("=========================")
+print("Biaya Jabatan    = 2587875")
+print("Iuran Pensiun    = 15500")
+print("Iuran organisasi = 3500")
+print(">>Gaji Netto     = 31875")
